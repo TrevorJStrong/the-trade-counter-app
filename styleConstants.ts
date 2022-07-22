7f0110d2871fd9b1f5c6607b6c8062382e175b43
TrevorJStrong <trevorstrong94@gmail.com>
2022-07-22 12:19:08 +0100

    feat: images & fonts & style constants added

---
+++ styleConstants.ts
@@ -0,0 +1,24 @@
+export const colours = {
+    primaryText: '#13262F',
+    secondary: '#F58F29',
+    grey: '#F4F5F8',
+}
+
+export const marginSizes = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 32,
+    xl: 40
+}
+
+export const fontSizes = {
+    xxs: 14,
+    xs: 16,
+    small: 18,
+    medium: 20,
+    large: 28,
+    xl: 32,
+    xxl: 44
+}
+  
