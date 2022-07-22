cce8d70aff99b37e69aa4ccb0a2ac827b8fdef21
TrevorJStrong <trevorstrong94@gmail.com>
2022-07-22 12:18:14 +0100

    feat: app setup

---
+++ App.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import TabNavigator from "./src/navigation";
+import { requestLocationPermissions } from './src/utils/location';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+// import useFonts from "./src/utils/hooks/useFonts";
+
+export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Keep the splash screen visible while we fetch resources
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          montserrat: require('./assets/fonts/Montserrat-Light.ttf')
+        });
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  const mainTheme = {
+    ...DefaultTheme,
+    colors: {
+      ...DefaultTheme.colors,
+      background: '#fff',
+    }
+  }
+
+  return (
+    <SafeAreaProvider
+      onLayout={onLayoutRootView}
+    >
+      <NavigationContainer theme={mainTheme}>
+        <TabNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+
+}
