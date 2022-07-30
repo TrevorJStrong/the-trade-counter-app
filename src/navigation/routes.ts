f1a8e5a1b46078676f4a432594c51c51751a0511
TrevorJStrong <trevorstrong94@gmail.com>
2022-07-30 11:17:15 +0100

    refactor: define route names in seperate file

---
+++ src/navigation/routes.ts
@@ -0,0 +1,7 @@
+export const HOME_SCREEN = 'Home';
+export const STORE_CATEGORY_SCREEN = 'StoreCategories';
+export const STORE_PRODUCTS_SCREEN = 'StoreProducts';
+
+export const BASKET_SCREEN = 'Basket';
+
+export const PROFILE_SCREEN = 'Profile';
